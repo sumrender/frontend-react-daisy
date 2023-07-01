e2de8abce6b2c023a0161a98ffe5a1da5440bd66
Sumrender <sumrenders@gmail.com>
2023-07-01 19:02:06 +0530

    center is working

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+      <button className="btn">Hello daisyUI</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
